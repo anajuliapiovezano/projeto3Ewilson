1bff20eca06983fde8f84802cef83b86b789206e
juli4sxl <106088100+anajuliapiovezano@users.noreply.github.com>
2024-03-12 08:47:50 -0300

    aaa

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const botoes =document.querySelectorAll(".botoes");
+
+for(let i = 0 <botoes.length; j++){
+    botoes[i].onclick = function(){
+        for (let j= 0; j< botoes.length; jj++){
+            botoes[j].classlist.remover("ativo")
+        }
+        botoes[i].classlist.add("ativo")
+    }
+}
