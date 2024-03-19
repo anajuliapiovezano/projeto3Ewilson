1fe5168a4145c7a713eee703389e29be06f5c892
juli4sxl <106088100+anajuliapiovezano@users.noreply.github.com>
2024-03-19 08:26:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const botoes =document.querySelectorAll(".botoes");
 
-for(let i = 0; i < botoes.length; j++){
+for(let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
         for (let j= 0; j< botoes.length; jj++){
-            botoes[j].classlist.remover("ativo")
+            botoes[j].classList.remove("ativo")
         }
-        botoes[i].classlist.add("ativo")
+        botoes[i].classList.add("ativo")
     }
 }
